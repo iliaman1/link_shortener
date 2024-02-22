9db8581eb660cc4aa69d8edcb6cb5c4cc242f788
iliaman <iliaman1993@gmail.com>
2024-02-23 02:48:11 +0300

    add url for redirection by token

--- SmartLink/api/shortener/urls.py
+++ SmartLink/api/shortener/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from apps.shortener.views import SmartUrlCreateView
-
+from apps.shortener.views import SmartUrlCreateView, SmartUrlRedirect
 
 urlpatterns = [
+    path('<str:token>', SmartUrlRedirect.as_view(), name='smarturl-redirect'),
     path('', SmartUrlCreateView.as_view(), name='smarturl-create')
 ]
